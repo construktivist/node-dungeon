4cfa613790cc9bcd16ce19a4ecd29c3981bf092a
Keegan Kelly <keegankelly1@gmail.com>
2019-02-08 13:04:41 -0600

    Made an object to handle dice rolls for action and stat resolutions.

---
+++ rolls.js
@@ -0,0 +1,29 @@
+var print = require('./printer.js');
+
+
+exports.roll = {
+  d20: function(){
+    return this.randomInt(1, 20);
+  },
+
+  d10: function(){
+    return this.randomInt(1, 10);
+  },
+
+  d8: function(){
+    return this.randomInt(1, 8);
+  },
+
+  d6: function(){
+    return this.randomInt(1, 6);
+  },
+
+  d4: function(){
+    return this.randomInt(1, 4);
+  },
+
+  randomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+};
