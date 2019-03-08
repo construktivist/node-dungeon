54daa47e73fc7fcc09a99d68c06f7d8b46eaea29
Keegan Kelly <keegankelly1@gmail.com>
2019-03-08 13:14:32 -0600

    Trying to create a function that will return the players options for each combat category.

--- combat/battlePrompts.js
+++ combat/battlePrompts.js
@@ -1,13 +1,31 @@
+var inquirer = require('inquirer');
+
+weapons = [`Sword`, `Bow`];
+
+// const printWeapons = (items) => {
+//     items.forEach(item => {
+//         return (item);
+//     });
+// };
+
 exports.actions = {
     prompt:  [
         {
           type: 'list',
           name: 'decision',
           message: 'What do you do?',
-          choices: [ "Use Weapon",
-              "Use Warrior Ability",
+          choices: [
+              new inquirer.Separator('=== Weapon ==='),
+              // printWeapons(weapons),
+              weapons[0],
+              weapons[1],
+              new inquirer.Separator('=== Warrior Abilities ==='),
+              "Use Power Attack",
+              new inquirer.Separator('=== Rogue Abilities ==='),
               "Use Rogue Ability",
+              new inquirer.Separator('=== Magic ==='),
               "Use Magic Ability",
+              new inquirer.Separator('=== Divine ==='),
               "Use Divine Ability"],
           filter: function(val) {
             return val.toLowerCase();
