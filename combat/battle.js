d3948de1411c4bdccb4435a0eec6a62bcbd501bb
Keegan Kelly <keegankelly1@gmail.com>
2019-03-18 14:14:20 -0500

    Removed inquire section from Battle and included it in battlePrompts.

--- combat/battle.js
+++ combat/battle.js
@@ -3,20 +3,15 @@ const battle = require(`./battlePrompts`);
 var Enemy = require('../enemies/enemy.js');
 var print = require('../helpers/printer.js');
 
+
 function Battle(enemy){
 
   this.enemy = new Enemy(enemy);
 
   this.run = () => {
     print.text.enemyStats(this.enemy);
-    this.prompt();
-  },
-
-  this.prompt = () => {
-    inquirer.prompt(battle.actions.prompt).then(answers => {
-      console.log(answers);
-    });
- }
+    this.battle.inquire();
+  }
 
 }
 
--- combat/battlePrompts.js
+++ combat/battlePrompts.js
@@ -1,37 +1,46 @@
 var inquirer = require('inquirer');
 var fs = require('fs');
 
-
+// var character = JSON.parse(fs.readFileSync('./data/character.js'));
 
 
 exports.actions = {
-    character: JSON.parse(fs.readFileSync('../data/character.js')),
 
-    prompt:  [
+    character: JSON.parse(fs.readFileSync('./data/character.js')),
+
+    questions:  [
         {
           type: 'list',
           name: 'decision',
           message: 'What do you do?',
           choices: [
               new inquirer.Separator('=== Weapon ==='),
-              this.character.weapons[0],
-              this.character.weapons[1],
+              character.weapons[0],
+              character.weapons[1],
               new inquirer.Separator('=== Warrior Abilities ==='),
-              this.character.warrior.abilities[0],
-              this.character.warrior.abilities[1],
+              character.warrior.abilities[0],
+              character.warrior.abilities[1],
               new inquirer.Separator('=== Rogue Abilities ==='),
-              this.character.rogue.abilities[0],
-              this.character.rogue.abilities[1],
+              character.rogue.abilities[0],
+              character.rogue.abilities[1],
               new inquirer.Separator('=== Magic ==='),
-              this.character.magic.abilities[0],
-              this.character.magic.abilities[1],
+              character.magic.abilities[0],
+              character.magic.abilities[1],
               new inquirer.Separator('=== Divine ==='),
-              this.character.divine.abilities[0],
-              this.character.divine.abilities[1]
+              character.divine.abilities[0],
+              character.divine.abilities[1]
           ],
           filter: function(val) {
             return val.toLowerCase();
           }
         }
-      ]
+    ],
+
+    inquire: () => () => {
+        console.log(this.character);
+        // inquirer.prompt(this.questions).then(answers => {
+        //     console.log(answers);
+        // });
+    }
+
 };
