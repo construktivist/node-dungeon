79fb8f50bf69eaeb400de462e40b24abcdd8f0cd
Keegan Kelly <keegankelly1@gmail.com>
2019-03-19 12:50:41 -0500

    Didn't need to have combat prompts in a separate module. Decided to put it all in Battle. This fixed the async of reading character data at app start.

--- combat/battle.js
+++ combat/battle.js
@@ -1,5 +1,5 @@
 const inquirer = require(`inquirer`);
-const battle = require(`./battlePrompts`);
+const fs = require(`fs`);
 var Enemy = require('../enemies/enemy.js');
 var print = require('../helpers/printer.js');
 
@@ -8,9 +8,41 @@ function Battle(enemy){
 
   this.enemy = new Enemy(enemy);
 
+  this.character = JSON.parse(fs.readFileSync('./data/character.js'));
+
+  this.questions = [
+        {
+          type: 'list',
+          name: 'decision',
+          message: 'What do you do?',
+          choices: [
+              new inquirer.Separator('=== Weapon ==='),
+              this.character.weapons[0],
+              this.character.weapons[1],
+              new inquirer.Separator('=== Warrior Abilities ==='),
+              this.character.warrior.abilities[0],
+              this.character.warrior.abilities[1],
+              new inquirer.Separator('=== Rogue Abilities ==='),
+              this.character.rogue.abilities[0],
+              this.character.rogue.abilities[1],
+              new inquirer.Separator('=== Magic ==='),
+              this.character.magic.abilities[0],
+              this.character.magic.abilities[1],
+              new inquirer.Separator('=== Divine ==='),
+              this.character.divine.abilities[0],
+              this.character.divine.abilities[1]
+          ],
+          filter: function(val) {
+            return val.toLowerCase();
+          }
+        }
+    ];
+
   this.run = () => {
     print.text.enemyStats(this.enemy);
-    this.battle.inquire();
+    inquirer.prompt(this.questions).then(answers => {
+        console.log(answers);
+    });
   }
 
 }
