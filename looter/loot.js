149f07d65acba168ba6f7f17760d9ba75003116e
Keegan Kelly <keegankelly1@gmail.com>
2019-07-10 13:16:02 -0500

    Added some more logic for looter.

--- looter/loot.js
+++ looter/loot.js
@@ -3,6 +3,15 @@ const dice = require(`../rolls.js`);
 
 exports.treasure = {
     gold: function(){
+        let roll = dice.roll.d20()
+
+            roll < 6 ? return treasure = dice.roll.d6()
+            :   roll < 11 ? return treasure = dice.roll.d8()
+            :   roll < 15 ? return treasure = dice.roll.d10()
+            :   roll <= 20 ? return treasure = dice.roll.d20();
+
+
+
         print.text.narration("You received 1 gold!")
-    } 
+    }, 
 }
